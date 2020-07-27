d79cfd0d5f9130b15a5b86368622edd46f919bf5
Felipe Santos <fdms@icomp.ufam.edu.br>
2020-07-27 19:43:52 -0400

    Revert "bot is alive"
    This reverts commit 54cd0765cc88e862bbfcee8c8367c960b7ee3757.

--- utils.py
+++
@@ -1,6 +0,0 @@
-from unicodedata import normalize
-
-#Cath body and ignore graphics accents
-def normalizeWords(msg):
-    msg = msg.lower()
-    return normalize('NFKD', msg).encode('ASCII', 'ignore').decode('ASCII')
